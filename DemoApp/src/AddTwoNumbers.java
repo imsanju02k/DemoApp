5a96937903ba4d2cce50efee1e03de8d26b206a7
Sanjay <imsanju2k@gmail.com>
2022-12-02 17:59:06 +0530

    first files

---
+++ DemoApp/src/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("hello java ");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
